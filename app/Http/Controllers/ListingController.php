792a0b7a0e7afac4b0393946ba51c7d363f75ca8
zhongxi <zhongxi710@gmail.com>
2024-07-14 17:17:44 +0900

    URL링크를 직접 사용하지 않고 ziggy로 route를 사용하게

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,12 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\List_;
 
 class ListingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return inertia(
@@ -20,17 +18,11 @@ class ListingController extends Controller
         );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return inertia('Listing/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $validatedData = $request->validate([
@@ -50,10 +42,6 @@ class ListingController extends Controller
             ->with('success', 'Listing was created!');
     }
 
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Listing $listing)
     {
         return inertia(
@@ -93,11 +81,11 @@ class ListingController extends Controller
             ->with('success', 'Listing was updated!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Listing $listing)
     {
-        //
+        $listing->delete();
+
+        return redirect()->back()
+            ->with('success', 'Listing was Deleted!');
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-
+    <title>SPA Study</title>
+    @routes
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
