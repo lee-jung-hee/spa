8d21577e56c4cc855c41570262a37d1baa5e0478
zhongxi <zhongxi710@gmail.com>
2024-07-18 00:04:03 +0900

    Tailwind 설치 및 살짝 추가함

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,33 +1,33 @@
 <template>
-    <Link href="/">Main Page</Link>&nbsp;
-    <Link href="/hello">Show Page</Link>&nbsp;
-    <Link href="/listing">List Page</Link>&nbsp;
-    <Link href="/listing/create">Create Page</Link>
-  <!-- <div>The page with time {{ timer }}</div> -->
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
-  <slot>Default</slot>
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+      <div class="container mx-auto">
+        <nav class="p-4 flex items-center justify-between">
+          <div class="text-lg font-medium">
+            <Link :href="route('listing.index')">목록</Link>
+          </div>
+          <div class="text-xl text-indigo-600 dark:text-indigo-600 font-bold">
+            <Link :href="route('listing.index')">오피스맵</Link>&nbsp;
+          </div>
+          <div class="text-lg font-medium">
+            <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-400 text-white font-medium p-2 rounded-md">+ 매물등록</Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <main class="container mx-auto p-4">
+      <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+        {{ flashSuccess }}
+      </div>
+      <slot>Default</slot>
+    </main>
 </template>
 
 <script setup>
 import { computed } from 'vue'
 import { Link, usePage } from '@inertiajs/vue3'
-// const x = ref(0)
-// const y = computed(() => x.value * 2)
+import { route } from 'ziggy';
 const page = usePage()
 const flashSuccess = computed(
   () => page.props.flash.success,
 )
-// import { ref } from 'vue'
-// const timer = ref(0)
-// setInterval(() => timer.value++, 1000)
 </script>
-
-<style scoped>
-  .success {
-    background-color: green;
-    color: white;
-  }
-</style>
-  
